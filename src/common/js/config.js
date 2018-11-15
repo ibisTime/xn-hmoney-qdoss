d52c9d2357f93138af08a4684bb21761fbad8742
hss1994 <1070798770@qq.com>
2018-11-15 10:43:23 +0800

    更换七牛云

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,8 +1,8 @@
 export const SYSTEM_CODE = 'CD-HPMN000024';
 // 七牛上传地址
-export const UPLOAD_URL = 'http://up-z0.qiniup.com';
+export const UPLOAD_URL = 'http://up.qiniup.com';
 // 七牛图片前缀
-export const PIC_PREFIX = 'http://pgf8juy5p.bkt.clouddn.com/';
+export const PIC_PREFIX = 'http://image.hp.hichengdai.com/';
 export const PIC_BASEURL_L = '?imageMogr2/auto-orient/thumbnail/!1000x1000r';
 
 // 系统userid
